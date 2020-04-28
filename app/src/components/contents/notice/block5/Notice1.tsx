a76936014d9719cd0c514b1b9576056bfd8659aa
KimHeeDong <eastshine94@naver.com>
2020-04-28 21:46:04 +0900

    add tab menu script

--- app/src/components/contents/notice/block5/Notice1.tsx
+++ app/src/components/contents/notice/block5/Notice1.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React,{useState} from 'react';
 import styled from 'styled-components';
 import { IR } from '../../../base/Base';
 
@@ -11,9 +11,8 @@ interface StyledProps {
 
 
 const SU = IR.SU;
-const Wrapper = styled.div.attrs((props: StyledProps) => ({
-    active: false,
-}))`
+const Wrapper = styled.div`
+    position: relative;
     border: 1px solid #ccc;
     padding: 8px;
     height: 105px;
@@ -28,35 +27,34 @@ const StyledList = styled.li.attrs((props: StyledProps) => ({
     active: false,
     bgColor: props.active ? "#2c94c4" : "inherit",
     fontColor: props.active ? "#fff" : "inherit",
+    isDisplay: props.active ? "block" : "none",
 }))`
-    position: relative;
     border: 1px solid #ccc;
     margin-right: -1px;
     margin-bottom: -1px;
     background-color: ${props => props.bgColor};
-    &:nth-child(1) ul{
-        display: block;
-    }
-    & a{
+
+    & div{
         padding: 5px 10px;
-        display: block;   
+        cursor: pointer;
     }
-    & > a {
+    & > div {
         color: ${props => props.fontColor};
     }
     
     & ul{       
-        display: none;
+        display: ${props => props.isDisplay};
         position: absolute;
         border: 0;
         width: 270px;
-        top: 38px;
-        left: 0;
+        top: 48px;
+        left: 10px;
         & li {
             border: 0;
             background: url("https://user-images.githubusercontent.com/41350459/80065043-6bd45700-8574-11ea-8c34-b555d66bd1d1.gif") no-repeat 0 8px;
-            padding-left: 18px;
+            padding-left: 10px;
             & a{
+                display: block;
                 overflow: hidden;
                 text-overflow: ellipsis;
                 white-space:nowrap;
@@ -69,27 +67,33 @@ const StyledList = styled.li.attrs((props: StyledProps) => ({
         }
     }
 `;
-const Notice1: React.SFC = () => {
 
+
+const Notice1: React.FC = () => {
+    const [isselected, setSelected] = useState([true, false, false]);
+    const onChangeSelect = (index:number) => {
+        const newSelected= isselected.map((v, i) => i === index? true:false);
+        setSelected(newSelected);
+    }
     return (
         <Wrapper>
             <h4><SU>탭 메뉴</SU></h4>
             <ul>
-                <StyledList active={true}><a href="#"> 공지사항1</a>
+                <StyledList active={isselected[0]}><div onClick={() => onChangeSelect(0)}> 공지사항1</div>
                     <ul>
                         <li><a href="#">첫 번째 공지사항 탭 메뉴 테스트 목록입니다. 첫 번째 공지사항 탭 메뉴 테스트 목록입니다.</a></li>
                         <li><a href="#">첫 번째 공지사항 탭 메뉴 테스트 목록입니다. 첫 번째 공지사항 탭 메뉴 테스트 목록입니다.</a></li>
                         <li><a href="#">첫 번째 공지사항 탭 메뉴 테스트 목록입니다. 첫 번째 공지사항 탭 메뉴 테스트 목록입니다.</a></li>
                     </ul>
                 </StyledList>
-                <StyledList><a href="#"> 공지사항2</a>
+                <StyledList active={isselected[1]}><div onClick={() => onChangeSelect(1)}> 공지사항2</div>
                     <ul>
                         <li><a href="#">두 번째 공지사항 탭 메뉴 테스트 목록입니다.</a></li>
                         <li><a href="#">두 번째 공지사항 탭 메뉴 테스트 목록입니다.</a></li>
                         <li><a href="#">두 번째 공지사항 탭 메뉴 테스트 목록입니다.</a></li>
                     </ul>
                 </StyledList>
-                <StyledList><a href="#"> 공지사항3</a>
+                <StyledList active={isselected[2]}><div onClick={() => onChangeSelect(2)}> 공지사항3</div>
                     <ul>
                         <li><a href="#">세 번째 공지사항 탭 메뉴 테스트 목록입니다.</a></li>
                         <li><a href="#">세 번째 공지사항 탭 메뉴 테스트 목록입니다.</a></li>
